af4d3979158fb6cf91e60a911b037ad16b98906e
swambach <62082148+swambach@users.noreply.github.com>
2022-05-27 11:58:37 -0400

    update

--- src/main/java/com/mindex/challenge/data/ReportingStructure.java
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -2,8 +2,21 @@ package com.mindex.challenge.data;
 
 public class ReportingStructure {
 	
-	private Employee emp;
+	private Employee employee;
 	private long numberOfReports;
+	
+	public Employee getEmployee() {
+		return employee;
+	}
+	public void setEmployee(Employee emp) {
+		this.employee = emp;
+	}
+	public long getNumberOfReports() {
+		return numberOfReports;
+	}
+	public void setNumberOfReports(long numberOfReports) {
+		this.numberOfReports = numberOfReports;
+	}
 	
 
 }
